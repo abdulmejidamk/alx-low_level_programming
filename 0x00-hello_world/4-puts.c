eaed2cf1aaa3ce07450aa8cd15920ba6e76e9059
abdulmejidamk <abdulmejidamk@gmail.com>
2023-03-16 00:51:24 -0700

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+
+#include <stdio.h>
+/**
+ * main -  this is ths first c program
+ *
+ * Return:  0 after finishing the execution
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
