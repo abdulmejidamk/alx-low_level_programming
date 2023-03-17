c895036a1e4f2ae377e02e1fcf3a26f76378d409
abdulmejidamk <abdulmejidamk@gmail.com>
2023-03-17 00:11:41 -0700

    Optional

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - outputs lowercase of alphabet entered
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int val = 48, i = 1;
+
+while (i < 10)
+{
+int val1 = 48 + i;
+while (val1 < 58)
+{
+	putchar(val);
+	putchar(val1++);
+	if (val == 56 && val1 == 58)
+		break;
+	putchar(',');
+	putchar(' ');
+}
+if (val == 56 && val1 == 58)
+break;
+val++;
+i++;
+}
+putchar('\n');
+return (0);
+}
