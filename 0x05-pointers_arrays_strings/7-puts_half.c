061f641f5e112c4efdd0bc6c687ae042ed84a80a
abdulmejidamk <abdulmejidamk@gmail.com>
2023-03-26 23:16:39 -0700

    putshalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string parameter
+ *
+ * Return: void
+ */
+void puts_half(char *str)
+{
+	int i = 0, j = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	if (i % 2 == 0)
+		j = i / 2;
+	else
+		j = (i + 1) / 2;
+
+	while (j < i)
+		_putchar(str[j++]);
+
+	_putchar('\n');
+}
