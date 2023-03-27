ed029b560f5ce510da6d7fbdad281a0489d3caab
abdulmejidamk <abdulmejidamk@gmail.com>
2023-03-26 22:53:40 -0700

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,11 @@
  * swap_int - swaps integers
  * @a: pointer to integer number 1
  * @b: pointer to integer number 2
- *
  * Return: void
  */
 void swap_int(int *a, int *b)
 {
 	int temp = *a;
-	*a=*b; 
+	*a = *b; 
 	*b = temp;
 }
