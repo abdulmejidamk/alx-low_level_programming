414072f452fe4065944aaa1d49be04074921d0ad
abdulmejidamk <abdulmejidamk@gmail.com>
2023-05-08 20:02:44 -0700

    com

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -5,6 +5,7 @@
  * @text_content: the text content that is going to be added to the file
  * Return: 1 if successful -1 if it fails
  */
+
 int create_file(const char *filename, char *text_content)
 {
 	int fd, wt, count;
@@ -13,16 +14,20 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+
 	count = 0;
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
 	if (fd == -1)
 	{
 		return (-1);
 	}
+
 	while (text_content[count] != '\0')
 	{
 		count++;
 	}
+
 	if (text_content != NULL)
 	{
 		wt = write(fd, text_content, count);
