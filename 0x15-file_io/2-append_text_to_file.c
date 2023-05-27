f69361452cf63fa0fcd0274e4e216129ce7d0616
abdulmejidamk <abdulmejidamk@gmail.com>
2023-05-27 06:36:27 -0700

    fixed append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,58 +1,33 @@
 #include "main.h"
-/**
- * length - determines the length of a string
- * @str: string to be counted
- * Return: length of the string
- */
-int length(char *str)
-{
-	int len;
-	len = 0;
 
-	while(str[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
 /**
- * append_text_to_file - appends text to our file
- * @filename: the file in to which our text is appended
- * @text_content: the text file to be appended
- * Return: 1 if successful otherwise -1
+ * append_text_to_file - appends text at the end of a file
+ * @filename: file to append the text to
+ * @text_content: content to append into the file
+ *
+ * Return: 1 on success and -1 on failure
  */
-	
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file, app;
+	int fd, a, b = 0;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
-	
-	file = open(filename, O_WRONLY | O_APPEND);
 
-	if (file == -1)
-	{
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd < 0)
 		return (-1);
-	}
-	
-	if (text_content != NULL)
-	{
-		app = write(file, text_content, length(text_content));
 
-		if (app == -1)
-		{
-			close(file);
+	if (text_content)
+	{
+		while (text_content[b])
+			b++;
+		a = write(fd, text_content, b);
+		if (a != b)
 			return (-1);
-		}
-		close(file);
-		return (1);
 	}
-	else
-	{
-		close(file);
-		return (-1);
-	}
+
+	close(fd);
+
+	return (1);
 }
